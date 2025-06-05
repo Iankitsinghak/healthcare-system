ea6c612fe08797009f18b116a7b67e1dc8743d5b
Ankit Singh <ankitsinghak9051@gmail.com>
2025-06-06 01:29:34 +0530

    Delete backend/corsConfig.js

--- backend/corsConfig.js
+++
@@ -1,9 +0,0 @@
-// corsConfig.js
-import cors from 'cors';
-
-const corsOptions = {
-  origin: '*', // testing ke liye sab allow, production mein frontend URL daalna better hai
-  credentials: true,
-};
-
-export default cors(corsOptions);
